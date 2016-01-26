cd1d0230957662036238c2412955da1c8201fb1a
fehu <lord.fehu@gmail.com>
2016-01-25 22:20:35 -0600

    using HLists;

--- src/N3.hs
+++ src/N3.hs
@@ -19,6 +19,7 @@ module N3 (
 
 ) where
 
+import Data.HList
 
 -----------------------------------------------------------------------------
 -- From https://downloads.haskell.org/~ghc/7.4.1/docs/html/users_guide/kind-polymorphism-and-promotion.html
@@ -49,18 +50,43 @@ instance VecElem Nat1 Nat1 where vecElem _ (Cons x _) = x
 vecElem1 :: (VecElem Nat1 vn) => Vec a vn -> a
 vecElem1 = vecElem (undefined :: SomeNat Nat1)
 
-
 -----------------------------------------------------------------------------
 
-data NElem = NElem
+--data NElem = NElem
+data NElem = NInput
+           | Neuron
+           | NOutput
 
 type NLayer n = Vec NElem n
 
+--type NLayers = HList
+
 --type PreviousLayers ln = Vec
 
-type SelectSynapseInput prev = prev -> NElem
+--type SelectSynapseInput prev = prev -> NElem
 
------------------------------------------------------------------------------
+type SelectSynapseInput prev = HList prev -> NElem
+
+--type EmptyNNet = HNil
+
+
+----------------------------------------------------------------------------
+
+--mkInputLayer ::
+
+
+
+mkNeuron :: HList prev -> Vec (SelectSynapseInput prev) n -> NElem
+mkNeuron = undefined
+
+--nextLayer :: HList prev -> Vec (SelectSynapseInput prev) n -> NLayer n
+--nextLayer l selVec = undefined
+
+----------------------------------------------------------------------------
+
+
+
+----------------------------------------------------------------------------
 
 
 foo :: Nat -> Vec1 a -> a
