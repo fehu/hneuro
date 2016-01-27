e7117d6cf63b614c50136398baa9977903ac29f9
fehu <lord.fehu@gmail.com>
2016-01-26 19:44:25 -0600

    some progress on N3 DSL;

--- src/N3.hs
+++ src/N3.hs
@@ -1,4 +1,10 @@
-{-# LANGUAGE GADTs, FlexibleContexts, TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE GADTs
+           , FlexibleContexts
+           , TypeSynonymInstances
+           , FlexibleInstances
+           , TypeOperators
+           , ExistentialQuantification
+       #-}
 
 
 -----------------------------------------------------------------------------
@@ -26,9 +32,18 @@ import Data.HList
 
 data Nat = Ze | Su Nat
 
-data Vec :: * -> Nat -> * where
-  Nil  :: Vec a Ze
-  Cons :: a -> Vec a n -> Vec a (Su n)
+data Vec :: Nat -> * -> * where
+  VNil  :: Vec Ze a
+  VCons :: a -> Vec n a -> Vec (Su n) a
+
+--type VecR a (n :: Nat) = Vec n a
+
+instance Functor (Vec n) where fmap f (VCons h t) = VCons (f h) (fmap f t)
+
+(+:) :: a -> Vec n a -> Vec (Su n) a
+(+:) = VCons
+
+--instance Foldable (Vec n) where foldr f b0 t =
 
 -----------------------------------------------------------------------------
 
@@ -38,26 +53,30 @@ type Nat2 = Su Nat1
 
 data SomeNat (n :: Nat)
 
-type Vec1 a = Vec a Nat1
-type Vec2 a = Vec a Nat2
+type Vec1 a = Vec Nat1 a
+type Vec2 a = Vec Nat2 a
 
 -----------------------------------------------------------------------------
 
-class VecElem (n :: Nat) (vn :: Nat) where vecElem :: SomeNat n -> Vec a vn -> a
+class VecElem (n :: Nat) (vn :: Nat) where vecElem :: SomeNat n -> Vec vn a -> a
 
-instance VecElem Nat1 Nat1 where vecElem _ (Cons x _) = x
+instance VecElem Nat1 Nat1 where vecElem _ (VCons x _) = x
 
-vecElem1 :: (VecElem Nat1 vn) => Vec a vn -> a
+vecElem1 :: (VecElem Nat1 vn) => Vec vn a -> a
 vecElem1 = vecElem (undefined :: SomeNat Nat1)
 
 -----------------------------------------------------------------------------
 
+data InputsSel prev = forall n . InputsSel (HList prev -> Vec n NElem)
+
+--type Inputs
+
 --data NElem = NElem
 data NElem = NInput
-           | Neuron
+           | forall n . Neuron (Vec n NElem)
            | NOutput
 
-type NLayer n = Vec NElem n
+type NLayer n = Vec n NElem
 
 --type NLayers = HList
 
@@ -65,7 +84,7 @@ type NLayer n = Vec NElem n
 
 --type SelectSynapseInput prev = prev -> NElem
 
-type SelectSynapseInput prev = HList prev -> NElem
+--type SelectSynapseInput prev = HList prev -> NElem
 
 --type EmptyNNet = HNil
 
@@ -74,30 +93,71 @@ type SelectSynapseInput prev = HList prev -> NElem
 
 --mkInputLayer ::
 
+--class Neuron n (in' :: Nat) where nInputs   :: n in' -> Vec in' NElem
+--                                  newNeuron :: Vec in' NElem -> n in'
 
+mkNeuron' :: HList prev -> InputsSel prev -> NElem
+mkNeuron' hl (InputsSel sel) = Neuron $ sel hl
 
-mkNeuron :: HList prev -> Vec (SelectSynapseInput prev) n -> NElem
-mkNeuron = undefined
+--nInputs (Neuron v) = v
+
+--data NeuronDescriptor (n :: Nat) = NeuronDescriptor (Vec n NElem)
+
+--instance Neuron NeuronDescriptor n where nInputs (NeuronDescriptor v) = v
+--                                         newNeuron = NeuronDescriptor
+
+--mkNeuron :: HList prev -> Vec (SelectSynapseInput prev) n -> NElem
+--mkNeuron = undefined
 
 --nextLayer :: HList prev -> Vec (SelectSynapseInput prev) n -> NLayer n
 --nextLayer l selVec = undefined
 
 ----------------------------------------------------------------------------
 
+inputsLayer :: SomeNat n -> HList '[NLayer n]
+inputsLayer = undefined -- TODO
 
+nextLayer :: HList prev -> Vec n (InputsSel prev) -> HList (NLayer n ': prev)
+nextLayer prev nsel = layer .*. prev
+    where layer = fmap (mkNeuron' prev) nsel
+
+
+--data LayersBuilder (nIn :: Nat) (nOut :: Nat) hl = LayersBuilder hl
+
+--newtype LayersBuilder hl = LayersBuilder hl
+--
+--
+--instance Functor LayersBuilder
+----    where fmap f (LayersBuilder hl) =
+--
+--instance Applicative LayersBuilder
+--
+--instance Monad LayersBuilder
+
+
+--test = do l1 <- inputLayer
+--          l2 <- nextLayer (HCons layer _ -> vecElem1 layer)
+
+
+test = undefined
+    where l1 = inputsLayer (undefined :: SomeNat Nat1)
+          l2 = nextLayer l1
+                $ InputsSel (\(HCons layer _) -> vecElem1 layer +: VNil) +: VNil
+
+          -- undefined
 
 ----------------------------------------------------------------------------
 
 
 foo :: Nat -> Vec1 a -> a
-foo _ (Cons x _) = x
+foo _ (VCons x _) = x
 
 a = foo Ze
 b = foo (Su Ze)
 
 -----------------------------------------------------------------------------
 
-type A = Vec Int (Su Ze)
+type A = Vec (Su Ze) Int
 
 nat1 :: Nat
 nat1 = Su Ze
